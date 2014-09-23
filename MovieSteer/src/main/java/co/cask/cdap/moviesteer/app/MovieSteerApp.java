45747b9d4c652d7bbcca9b84f81e0d91b1ca49d2
Rohit Sinha <rsinha@continuuity.com>
2014-09-22 19:20:56 -0700

    MovieSteer: Added service to upload movies info

--- MovieSteer/src/main/java/co/cask/cdap/moviesteer/app/MovieSteerApp.java
+++ MovieSteer/src/main/java/co/cask/cdap/moviesteer/app/MovieSteerApp.java
@@ -28,6 +28,7 @@ import co.cask.cdap.api.annotation.Handle;
 import co.cask.cdap.api.annotation.ProcessInput;
 import co.cask.cdap.api.annotation.UseDataSet;
 import co.cask.cdap.api.app.AbstractApplication;
+import co.cask.cdap.api.common.Bytes;
 import co.cask.cdap.api.data.stream.Stream;
 import co.cask.cdap.api.dataset.lib.ObjectStore;
 import co.cask.cdap.api.dataset.lib.ObjectStores;
@@ -39,20 +40,30 @@ import co.cask.cdap.api.procedure.AbstractProcedure;
 import co.cask.cdap.api.procedure.ProcedureRequest;
 import co.cask.cdap.api.procedure.ProcedureResponder;
 import co.cask.cdap.api.procedure.ProcedureResponse;
+import co.cask.cdap.api.service.AbstractService;
+import co.cask.cdap.api.service.http.AbstractHttpServiceHandler;
+import co.cask.cdap.api.service.http.HttpServiceRequest;
+import co.cask.cdap.api.service.http.HttpServiceResponder;
 import co.cask.cdap.api.spark.AbstractSpark;
 import co.cask.cdap.api.spark.SparkSpecification;
 import co.cask.cdap.internal.io.UnsupportedTypeException;
+import io.netty.handler.codec.http.HttpResponseStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.util.UUID;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
 
 /**
  * MoveSteer: Movie Recommendation App based on Spark's MLibs using MovieLens dataset
  */
 public class MovieSteerApp extends AbstractApplication {
+
+  public static final Charset CHARSET_UTF8 = Charset.forName("UTF-8");
 
   @Override
   public void configure() {
@@ -62,15 +73,18 @@ public class MovieSteerApp extends AbstractApplication {
     addFlow(new RatingsFlow());
     addSpark(new MovieSteerSpecification());
     addProcedure(new PredictionProcedure());
+    addService(new MoviesUploadService());
 
     try {
       ObjectStores.createObjectStore(getConfigurer(), "ratings", String.class);
       ObjectStores.createObjectStore(getConfigurer(), "predictions", String.class);
+      ObjectStores.createObjectStore(getConfigurer(), "movies", String.class);
     } catch (UnsupportedTypeException e) {
       // This exception is thrown by ObjectStore if its parameter type cannot be
       // (de)serialized (for example, if it is an interface and not a class, then there is
       // no auto-magic way deserialize an object.) In this case that will not happen
       // because String is an actual class.
+      System.out.println("Catched exception");
       throw new RuntimeException(e);
     }
   }
@@ -129,6 +143,55 @@ public class MovieSteerApp extends AbstractApplication {
         .connect()
         .fromStream("ratingsStream").to("reader")
         .build();
+    }
+  }
+
+  public class MoviesUploadService extends AbstractService {
+    @Override
+    protected void configure() {
+      setName("MovieUploadService");
+      setDescription("Service to store moviesStore information to dataset");
+      useDataset("movies");
+      addHandler(new MoviesUploadHandler());
+    }
+  }
+
+  public class MoviesUploadHandler extends AbstractHttpServiceHandler {
+
+    @UseDataSet("movies")
+    private ObjectStore<String> moviesStore;
+
+    @Path("uploadmovies")
+    @POST
+    public void uploadHandler(HttpServiceRequest request, HttpServiceResponder responder) {
+
+      ByteBuffer requestContents = request.getContent();
+      if (requestContents == null) {
+        responder.sendError(HttpResponseStatus.NO_CONTENT.code(), "Movies information is empty.");
+        return;
+      }
+
+      String moviesData = CHARSET_UTF8.decode(requestContents).toString();
+
+      if (parseAndStoreMovies(moviesData)) {
+        responder.sendJson(HttpResponseStatus.OK.code(), "Movies information stored in dataset successfully.");
+      } else {
+        responder.sendError(HttpResponseStatus.BAD_REQUEST.code(), "Malformed movies information.");
+      }
+    }
+
+    private boolean parseAndStoreMovies(String moviesData) {
+      boolean validRequest = false;
+      String[] movies = moviesData.split("[\\r\\n]+");
+
+      for (String movie : movies) {
+        String[] movieInfo = movie.split("::");
+        if (!movieInfo[0].isEmpty() && !movieInfo[1].isEmpty()) {
+          validRequest = true;
+          moviesStore.write(Bytes.toBytes(movieInfo[0]), movieInfo[1]);
+        }
+      }
+      return validRequest;
     }
   }
 
