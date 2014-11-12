0c0fe0976b25eb962e8ac12ae2d86ae4372ce1d0
Taras Yatskiv <tyatskiv@cask.co>
2014-11-12 21:05:32 +0200

    CDAP-540: Updated response to the POJO.

--- MovieRecommender/src/main/java/co/cask/cdap/apps/movierecommender/MovieRecommenderServiceHandler.java
+++ MovieRecommender/src/main/java/co/cask/cdap/apps/movierecommender/MovieRecommenderServiceHandler.java
@@ -24,9 +24,6 @@ import co.cask.cdap.api.dataset.lib.ObjectStore;
 import co.cask.cdap.api.service.http.AbstractHttpServiceHandler;
 import co.cask.cdap.api.service.http.HttpServiceRequest;
 import co.cask.cdap.api.service.http.HttpServiceResponder;
-import com.google.gson.Gson;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import org.apache.spark.mllib.recommendation.Rating;
 
@@ -38,8 +35,6 @@ import javax.ws.rs.PathParam;
  * Handler that exposes HTTP API to retrieve recommended movies.
  */
 public class MovieRecommenderServiceHandler extends AbstractHttpServiceHandler {
-  private static final Gson GSON = new Gson();
-
   @UseDataSet("recommendations")
   private ObjectStore<Rating> recommendations;
 
@@ -72,7 +67,7 @@ public class MovieRecommenderServiceHandler extends AbstractHttpServiceHandler {
           return;
         }
 
-        responder.sendJson(HttpResponseStatus.OK.code(), prepareResponse(movies, userRatings, userPredictions));
+        responder.sendJson(prepareResponse(movies, userRatings, userPredictions));
       } finally {
         userPredictions.close();
       }
@@ -82,33 +77,28 @@ public class MovieRecommenderServiceHandler extends AbstractHttpServiceHandler {
   }
 
   /**
-   * Prepares a json string of watched and recommended movies in the following format:
+   * Prepares a response of watched and recommended movies in the following format:
    * {"rated":["ratedMovie1","ratedMovie2"],"recommended":["recommendedMovie1","recommendedMovie2"]}
    *
+   * @param store dataset with stored movies
    * @param userRatings user given rating to movies
    * @param userPredictions movie recommendation to user with predicted rating
    *
-   * @return {@link com.google.gson.JsonObject} of watched and recommended movies
+   * @return {@link Recommendation} of watched and recommended movies
    */
-  private JsonObject prepareResponse(ObjectStore<String> store,
-                                     CloseableIterator<KeyValue<byte[], UserScore>> userRatings,
-                                     CloseableIterator<KeyValue<byte[], Rating>> userPredictions) {
-    JsonArray watched = new JsonArray();
+  private Recommendation prepareResponse(ObjectStore<String> store,
+                                         CloseableIterator<KeyValue<byte[], UserScore>> userRatings,
+                                         CloseableIterator<KeyValue<byte[], Rating>> userPredictions) {
+    Recommendation recommendations = new Recommendation();
+
     while (userRatings.hasNext()) {
-      UserScore curRating = userRatings.next().getValue();
-      watched.add(GSON.toJsonTree(store.read(Bytes.toBytes(curRating.getMovieID()))));
+      recommendations.addRated(store.read(Bytes.toBytes(userRatings.next().getValue().getMovieID())));
     }
 
-    JsonArray recommended = new JsonArray();
     while (userPredictions.hasNext()) {
-      Rating curPrediction = userPredictions.next().getValue();
-      recommended.add(GSON.toJsonTree(store.read(Bytes.toBytes(curPrediction.product()))));
+      recommendations.addRecommended(store.read(Bytes.toBytes(userPredictions.next().getValue().product())));
     }
 
-    JsonObject response = new JsonObject();
-    response.add("rated", watched);
-    response.add("recommended", recommended);
-
-    return response;
+    return recommendations;
   }
 }
